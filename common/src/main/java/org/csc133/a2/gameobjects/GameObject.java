686edf4e2e688191afe5a7293193d27edd43bc28
MudK01 <khalid.mueed01@gmail.com>
2022-02-27 20:46:42 -0800

    Created Drawable and Steerable interfaces in interface subpackage and created Fixed and Moveable classes. Not sure if they should have their own files. Additionally, added required methods to each interface.

---
+++ common/src/main/java/org/csc133/a2/gameobjects/GameObject.java
@@ -0,0 +1,13 @@
+package org.csc133.a2.gameobjects;
+
+public class GameObject {
+}
+// use Dimension to encapsulate width and heights of objects
+
+class Fixed extends GameObject {
+
+}
+
+class Moveable extends GameObject {
+
+}
