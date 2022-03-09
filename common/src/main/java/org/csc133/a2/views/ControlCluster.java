180d5656364204ef51a6e9b02a66e698e8103a37
MudK01 <khalid.mueed01@gmail.com>
2022-03-09 14:27:18 -0800

    Still working on getting ControlCluster buttons oriented in correct spots.

--- common/src/main/java/org/csc133/a2/views/ControlCluster.java
+++ common/src/main/java/org/csc133/a2/views/ControlCluster.java
@@ -3,10 +3,13 @@ package org.csc133.a2.views;
 import com.codename1.charts.util.ColorUtil;
 import com.codename1.ui.Button;
 import com.codename1.ui.Container;
+import com.codename1.ui.Form;
 import com.codename1.ui.Label;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.GridLayout;
 import org.csc133.a2.GameWorld;
+
+import javax.swing.border.Border;
 
 public class ControlCluster extends Container {
     GameWorld gw;
@@ -17,8 +20,11 @@ public class ControlCluster extends Container {
         this.getStyle().setBgColor(ColorUtil.rgb(255,255,255));
         this.getStyle().setBgTransparency(255);
 
-        this.setLayout(new BorderLayout());
 
+        this.setLayout(new BorderLayout());
+        Form cluster = new Form(new BorderLayout());
+
+        ((BorderLayout)this.getLayout()).setCenterBehavior(BorderLayout.CENTER_BEHAVIOR_CENTER);
         left = new Button("Left");
         right = new Button("Right");
         fight = new Button("Fight");
@@ -26,11 +32,12 @@ public class ControlCluster extends Container {
         drink = new Button("Drink");
         brake = new Button("Brake");
         accelerate = new Button("Accel");
-        this.add(BorderLayout.WEST, left);
-        this.add(BorderLayout.WEST, right);
-        this.add(BorderLayout.WEST, fight);
-        this.add(BorderLayout.CENTER, exit);
-        this.add(BorderLayout.EAST, accelerate);
+        this.add(BorderLayout.WEST, left)
+                .add(BorderLayout.WEST, right)
+                .add(BorderLayout.WEST, fight)
+                .add(BorderLayout.CENTER, exit)
+                .add(BorderLayout.EAST, accelerate);
+//        this.add(BorderLayout.SOUTH, cluster);
 
 
     }
