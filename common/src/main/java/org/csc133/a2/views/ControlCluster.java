996a0d4e9ad006b4b75ebda3c40f0598449179dd
MudK01 <khalid.mueed01@gmail.com>
2022-03-09 18:45:27 -0800

    Fixed ControlCluster by creating 3 containers and adding each button to them, then to the layout.

--- common/src/main/java/org/csc133/a2/views/ControlCluster.java
+++ common/src/main/java/org/csc133/a2/views/ControlCluster.java
@@ -13,7 +13,9 @@ import javax.swing.border.Border;
 
 public class ControlCluster extends Container {
     GameWorld gw;
-    Button left, right, fight, exit, drink, brake, accelerate ;
+    Button left, right, fight, exit, drink, brake, accelerate;
+    Container leftCont, rightCont, fightCont, exitCont, drinkCont, brakeCont,
+            accelCont;
     public ControlCluster(GameWorld gw) {
         this.gw = gw;
 
@@ -22,9 +24,17 @@ public class ControlCluster extends Container {
 
 
         this.setLayout(new BorderLayout());
-        Form cluster = new Form(new BorderLayout());
+//        Form cluster = new Form(new BorderLayout());
 
-        ((BorderLayout)this.getLayout()).setCenterBehavior(BorderLayout.CENTER_BEHAVIOR_CENTER);
+//        ((BorderLayout)this.getLayout()).setCenterBehavior(BorderLayout.CENTER_BEHAVIOR_CENTER)CENTER_BEHAVIOR_CENTER;
+        leftCont = new Container(new BorderLayout());
+        rightCont = new Container(new BorderLayout());
+        fightCont = new Container(new BorderLayout());
+        exitCont = new Container(new BorderLayout());
+        drinkCont = new Container(new BorderLayout());
+        brakeCont = new Container(new BorderLayout());
+        accelCont = new Container(new BorderLayout());
+
         left = new Button("Left");
         right = new Button("Right");
         fight = new Button("Fight");
@@ -32,13 +42,21 @@ public class ControlCluster extends Container {
         drink = new Button("Drink");
         brake = new Button("Brake");
         accelerate = new Button("Accel");
-        this.add(BorderLayout.WEST, left)
-                .add(BorderLayout.WEST, right)
-                .add(BorderLayout.WEST, fight)
-                .add(BorderLayout.CENTER, exit)
-                .add(BorderLayout.EAST, accelerate);
-//        this.add(BorderLayout.SOUTH, cluster);
-
+//        this.add(BorderLayout.WEST, left)
+//                .add(BorderLayout.WEST, right)
+//                .add(BorderLayout.WEST, fight)
+//                .add(BorderLayout.CENTER, exit)
+//                .add(BorderLayout.EAST, accelerate);
+        leftCont.add(BorderLayout.WEST, left);
+        leftCont.add(BorderLayout.CENTER, right);
+        leftCont.add(BorderLayout.EAST, fight);
+        exitCont.add(BorderLayout.CENTER, exit);
+        rightCont.add(BorderLayout.WEST, drink);
+        rightCont.add(BorderLayout.CENTER, brake);
+        rightCont.add(BorderLayout.EAST, accelerate);
+        this.add(BorderLayout.WEST, leftCont);
+        this.add(BorderLayout.CENTER, exitCont);
+        this.add(BorderLayout.EAST, rightCont);
 
     }
 
