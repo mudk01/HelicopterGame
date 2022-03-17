af391ef81c00c49c12a8cb8ad15720a80642e095
MudK01 <khalid.mueed01@gmail.com>
2022-03-17 01:04:15 -0700

    Added Functionality to AccelerateCommand and DecelerateCommand

--- common/src/main/java/org/csc133/a2/commands/AccelerateCommand.java
+++ common/src/main/java/org/csc133/a2/commands/AccelerateCommand.java
@@ -1,4 +1,19 @@
 package org.csc133.a2.commands;
 
-public class AccelerateCommand {
+import com.codename1.ui.Command;
+import com.codename1.ui.events.ActionEvent;
+import org.csc133.a2.GameWorld;
+
+public class AccelerateCommand extends Command {
+    private GameWorld gw;
+
+    public AccelerateCommand(GameWorld gw) {
+        super("Accel");
+        this.gw = gw;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        gw.accelerateHeli();
+    }
 }
--- common/src/main/java/org/csc133/a2/commands/DecelerateCommand.java
+++ common/src/main/java/org/csc133/a2/commands/DecelerateCommand.java
@@ -1,4 +1,19 @@
 package org.csc133.a2.commands;
 
-public class DecelerateCommand {
+import com.codename1.ui.Command;
+import com.codename1.ui.events.ActionEvent;
+import org.csc133.a2.GameWorld;
+
+public class DecelerateCommand extends Command {
+    private GameWorld gw;
+
+    public DecelerateCommand(GameWorld gw) {
+        super("Brake");
+        this.gw = gw;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        gw.decelerateHeli();
+    }
 }
