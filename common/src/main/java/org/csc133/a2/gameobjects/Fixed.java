eb2d09d726f62fc612ce597d46aa8132d1d4aa55
MudK01 <khalid.mueed01@gmail.com>
2022-03-21 23:47:04 -0700

    Uncommented Fixed and moveable and currently planning on how to implement them.

--- common/src/main/java/org/csc133/a2/gameobjects/Fixed.java
+++ common/src/main/java/org/csc133/a2/gameobjects/Fixed.java
@@ -1,15 +1,8 @@
 package org.csc133.a2.gameobjects;
 
-import com.codename1.ui.Graphics;
-import com.codename1.ui.geom.Dimension;
-import com.codename1.ui.geom.Point;
+public abstract class Fixed extends GameObject {
+    public Fixed() {
+        super();
+    }
+}
 
-//Pointabstract class Fixed extends GameObject {
-//    public Fixed(Point location, Dimension dimension, int color) {
-//        super(location, dimension, color);
-//    }
-//
-//    public Fixed() {
-//        super();
-//    }
-//}
--- common/src/main/java/org/csc133/a2/gameobjects/Moveable.java
+++ common/src/main/java/org/csc133/a2/gameobjects/Moveable.java
@@ -1,22 +1,14 @@
-//package org.csc133.a2.gameobjects;
-//
-//import com.codename1.ui.geom.Dimension;
-//import com.codename1.ui.geom.Point;
-//
-//abstract class Moveable extends GameObject {
-//    private int speed;
-//    //compass angle
-//    private int heading;
-//
-//    public Moveable(Point location, Dimension dimension, int color) {
-//        super(location, dimension, color);
-//    }
-//
-//    public Moveable() {
-//        super();
-//    }
-//
-//    void move() {
-//
-//    }
-//}
+package org.csc133.a2.gameobjects;
+
+import com.codename1.ui.geom.Dimension;
+import com.codename1.ui.geom.Point;
+
+abstract class Moveable extends GameObject {
+
+    public Moveable() {
+        super();
+    }
+
+    void move() {
+    }
+}
