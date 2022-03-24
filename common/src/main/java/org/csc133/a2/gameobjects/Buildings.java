606bea48df59e2e799da495f14d89ec4deaa091a
MudK01 <khalid.mueed01@gmail.com>
2022-03-24 01:23:29 -0700

    Created Buildings class to for GameObjectCollection of Building objects.

---
+++ common/src/main/java/org/csc133/a2/gameobjects/Buildings.java
@@ -0,0 +1,23 @@
+package org.csc133.a2.gameobjects;
+
+import com.codename1.charts.util.ColorUtil;
+import com.codename1.ui.Graphics;
+import com.codename1.ui.geom.Point;
+
+import java.util.ArrayList;
+
+public class Buildings extends GameObjectCollection<Building>{
+
+    public Buildings() {
+        super();
+        this.color = ColorUtil.rgb(255,0,0);
+    }
+
+    @Override
+    public void draw(Graphics g, Point containerOrigin) {
+        for(Building building : getGameObjects()) {
+            building.draw(g, containerOrigin);
+        }
+    }
+
+}
