280ba1588d25ebf1deb059f01c2d57f70a4afc54
MudK01 <khalid.mueed01@gmail.com>
2022-03-25 22:08:23 -0700

    Created ButtonMaker method and removed old create new button and set command statements.

--- common/src/main/java/org/csc133/a2/views/ControlCluster.java
+++ common/src/main/java/org/csc133/a2/views/ControlCluster.java
@@ -1,10 +1,7 @@
 package org.csc133.a2.views;
 
 import com.codename1.charts.util.ColorUtil;
-import com.codename1.ui.Button;
-import com.codename1.ui.Container;
-import com.codename1.ui.Form;
-import com.codename1.ui.Label;
+import com.codename1.ui.*;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.GridLayout;
 import org.csc133.a2.GameWorld;
@@ -22,27 +19,16 @@ public class ControlCluster extends Container {
         this.getStyle().setBgColor(ColorUtil.rgb(0,0,0));
 
         leftCont = new Container(new BorderLayout());
-        leftCont.getStyle().setBgTransparency(255);
         rightCont = new Container(new BorderLayout());
-        rightCont.getStyle().setBgTransparency(255);
         exitCont = new Container(new BorderLayout());
-        exitCont.getStyle().setBgTransparency(255);
 
-        left = new Button("Left");
-        right = new Button("Right");
-        fight = new Button("Fight");
-        exit = new Button("Exit");
-        drink = new Button("Drink");
-        brake = new Button("Brake");
-        accelerate = new Button("Accel");
-
-        left.setCommand(new SteerLeftCommand(gw));
-        right.setCommand(new SteerRightCommand(gw));
-        fight.setCommand(new FightCommand(gw));
-        exit.setCommand(new ExitCommand(gw));
-        drink.setCommand(new DrinkCommand(gw));
-        brake.setCommand(new DecelerateCommand(gw));
-        accelerate.setCommand(new AccelerateCommand(gw));
+        left = this.buttonMaker(new SteerLeftCommand(gw), "Left");
+        right = this.buttonMaker(new SteerRightCommand(gw), "Right");
+        fight = this.buttonMaker(new FightCommand(gw), "Fight");
+        exit = this.buttonMaker(new ExitCommand(gw), "Exit");
+        drink = this.buttonMaker(new DrinkCommand(gw), "Drink");
+        brake = this.buttonMaker(new DecelerateCommand(gw), "Brake");
+        accelerate = this.buttonMaker(new AccelerateCommand(gw), "Accel");
 
         leftCont.add(BorderLayout.WEST, left);
         leftCont.add(BorderLayout.CENTER, right);
@@ -55,6 +41,13 @@ public class ControlCluster extends Container {
         this.add(BorderLayout.CENTER, exitCont);
         this.add(BorderLayout.EAST, rightCont);
 
+    }
+
+    Button buttonMaker(Command cmd, String label){
+        Button button = new Button(label);
+        button.setCommand(cmd);
+        button.getAllStyles().setBgTransparency(255);
+        return button;
     }
 
 }
