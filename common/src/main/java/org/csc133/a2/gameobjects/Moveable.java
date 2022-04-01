ff866c12dc1f4092efa419142f0643d710d939ae
MudK01 <khalid.mueed01@gmail.com>
2022-03-31 21:07:58 -0700

    created speed and heading integer variables, added getHeading methods to return heading.

--- common/src/main/java/org/csc133/a2/gameobjects/Moveable.java
+++ common/src/main/java/org/csc133/a2/gameobjects/Moveable.java
@@ -1,9 +1,8 @@
 package org.csc133.a2.gameobjects;
 
-import com.codename1.ui.geom.Dimension;
-import com.codename1.ui.geom.Point;
-
 abstract class Moveable extends GameObject {
+    int speed;
+    int heading;
 
     public Moveable() {
         super();
@@ -11,4 +10,18 @@ abstract class Moveable extends GameObject {
 
     public void move() {
     }
+
+    public int getHeading(double angle) {
+        this.heading = (int)(Math.round(Math.toDegrees(angle)));
+        if(heading < 0) {
+            heading += 360;
+        }
+        else if(heading >= 360) {
+            heading -= 360;
+        }
+
+        return heading;
+    }
+
+    public abstract int getHeading();
 }
